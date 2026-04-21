1c9f61493ff07751f80c8988d8ff02df16d70301
aabdelghany <ahmedabdelghany15@gmail.com>
2026-04-22 00:13:34 +0200

    Multiday adapter: drop redundant setFont (multiday.h sets its own)

--- WatchySim/WatchFaces/Multiday/Watchy_Multiday.cpp
+++ WatchySim/WatchFaces/Multiday/Watchy_Multiday.cpp
@@ -2,7 +2,6 @@
 
 #include "../../../../watchy-screens/src/faces/frame.h"
 #include "../../../../watchy-screens/src/faces/multiday.h"
-#include "../../../../watchy-screens/src/fonts/RainyHearts6pt7b.h"
 #include "../../../../watchy-screens/src/mock/mock_data.h"
 
 static MockState sim_mockMultiday;
@@ -12,9 +11,6 @@ WatchyMultiday::WatchyMultiday() {}
 void WatchyMultiday::drawWatchFace() {
     display.fillScreen(GxEPD_WHITE);
     drawWatchyChrome(display);
-
-    display.setFont(&rainyhearts6pt7b);
-
     sim_mockMultiday.tick();
     drawMultidayFace(display, 12, 32, sim_mockMultiday.currentMultiday());
 }
